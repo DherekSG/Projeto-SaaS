79f0c7fc930307f50acc10be3444ac5d57378ca3
DherekSG <dhereksgl@gmail.com>
2025-05-19 15:39:51 -0500

    Criacao do conexao.php

---
+++ backend/conexao.php
@@ -0,0 +1,11 @@
+<?php
+// conexão do banco de dados
+$host = "localhost";
+$dbname = "projeto_SaaS";
+$user = "root";
+$pass = "admin123"; // COLOCA A SENHA AQUI CABAÇO
+
+$coon = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$coon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "Conexão realizada com sucesso!";
+?>
