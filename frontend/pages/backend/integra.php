3775171eae2ce9ba3695b66e4a61b77afeecd53c
Gucostaa <gumoraiscosta@gmail.com>
2025-06-06 16:43:07 -0300

    Correção do commit feito pelo dherek

--- frontend/pages/backend/integra.php
+++
@@ -1,8 +0,0 @@
-<?php  
-$conn = pg_connect("host=localhost dbname=postgres user=postgres password=admin123");
-
-if(!$conn) {
-    echo "Erro na Conexão com o banco de dados.";
-} else {
-    echo "Conexão realizada com sucesso.";
-}
